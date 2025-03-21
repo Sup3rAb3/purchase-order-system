722c2931113d3f5d095e843165f8a436698a8370
Sup3rAb3 <abrahammanda.ac@gmail.com>
2025-03-21 12:09:02 +0200

    added login pages

--- purchase_order/settings.py
+++ purchase_order/settings.py
@@ -76,6 +76,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'allauth.account.middleware.AccountMiddleware',  # Add this line
 ]
 
 ROOT_URLCONF = 'purchase_order.urls'
