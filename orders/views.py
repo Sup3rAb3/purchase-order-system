aa764f1466e77fce7e13252bd86ad4398bec9f8f
Sup3rAb3 <abrahammanda.ac@gmail.com>
2025-03-24 10:13:09 +0200

    update views.py with signatories

--- orders/views.py
+++ orders/views.py
@@ -3,6 +3,11 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from .models import PurchaseOrder, PurchaseOrderItem
 from .forms import PurchaseOrderForm, PurchaseOrderItemFormSet
+from django.core.mail import send_mail
+from django.urls import reverse
+from django.utils.html import strip_tags
+from django.template.loader import render_to_string
+
 
 @login_required
 def create_purchase_order(request):
@@ -13,8 +18,8 @@ def create_purchase_order(request):
         if po_form.is_valid() and item_formset.is_valid():
             # Save the PurchaseOrder first
             purchase_order = po_form.save(commit=False)
-            purchase_order.requester = request.user  # Set the requester to the currently logged-in user
-            purchase_order.status = 'Pending'  # Set initial status
+            purchase_order.requester = request.user # Set the requester to the currently logged-in user
+            purchase_order.status = 'Pending' # Set initial status
             purchase_order.save()
 
             # Save the PurchaseOrderItem instances
@@ -22,6 +27,28 @@ def create_purchase_order(request):
             for item in items:
                 item.purchase_order = purchase_order  # Assign the PurchaseOrder to each item
                 item.save()
+
+            # Calculate the total amount
+            purchase_order.calculate_totals()
+
+            # Determine the signatories based on the total amount
+            if purchase_order.total_amount < 50000:
+                signatories = Signatory.objects.filter(role='Level1')[:2]  # Two Level 1 signatories
+            else:
+                signatories = Signatory.objects.filter(role='Level2')[:3]  # Three Level 2 signatories
+
+            # Send emails to signatories
+            for signatory in signatories:
+                subject = f"Purchase Order Approval Required: {purchase_order.purchase_order_number}"
+                context = {
+                    'purchase_order': purchase_order,
+                    'signatory': signatory,
+                    'approve_url': request.build_absolute_uri(reverse('approve_purchase_order', args=[purchase_order.id])),
+                    'deny_url': request.build_absolute_uri(reverse('deny_purchase_order', args=[purchase_order.id])),
+                }
+                html_message = render_to_string('emails/approval_email.html', context)
+                plain_message = strip_tags(html_message)
+                send_mail(subject, plain_message, 'amanda@corpus.co.zm', [signatory.email], html_message=html_message)
 
             messages.success(request, 'Your purchase order has been successfully submitted!')
             return redirect('create_purchase_order')
